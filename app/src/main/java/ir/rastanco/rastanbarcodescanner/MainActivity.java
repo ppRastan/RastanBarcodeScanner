25e4e765797e53d68c8c5c7408309575be0d717a
CafeWriter <samaneh.samiee@gmail.com>
2015-12-16 15:29:23 +0330

    Min SDK upgraded to 11

--- app/src/main/java/ir/rastanco/rastanbarcodescanner/MainActivity.java
+++ app/src/main/java/ir/rastanco/rastanbarcodescanner/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //my commit
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
