ee1a8319358e16655683921ddde107d221d74293
shaisteS <shs.woem@gmail.com>
2015-12-19 20:38:20 +0330

    fix error in Main Activity

--- app/src/main/java/ir/rastanco/rastanbarcodescanner/presenter/MainActivity.java
+++ app/src/main/java/ir/rastanco/rastanbarcodescanner/presenter/MainActivity.java
@@ -7,24 +7,22 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 
-import java.util.ArrayList;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import ir.rastanco.rastanbarcodescanner.R;
-import ir.rastanco.rastanbarcodescanner.presenter.SheredFiles;
 
 /*
 created by parisaRashidi  on 94/9/27
@@ -33,10 +31,9 @@ created by parisaRashidi  on 94/9/27
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-
     private String state = "default";
     private Toolbar toolbar;
-    private ImageButton sortFiles ;
+    private ImageButton sortFiles;
     private ImageButton change_image_sort;
     private ImageButton delete_btn;
     private ImageButton share_btn;
@@ -45,23 +42,30 @@ public class MainActivity extends AppCompatActivity
     private LinearLayout container;
     private LinearLayout temp_linear_for_checkbox;
     private Button select_all_checkboxes;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @TargetApi(Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
-        {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
             getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
         }
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setBackgroundTintList(getColorStateList(R.color.colorPrimary));
+       // fab.setBackgroundTintList(getColorStateList(R.color.colorPrimary));
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent iBarcodeReader=new Intent(MainActivity.this,BarcodeReadingActivity.class);
+                startActivity(iBarcodeReader);
             }
         });
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -71,14 +75,14 @@ public class MainActivity extends AppCompatActivity
         toggle.syncState();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        temp_linear_for_checkbox = (LinearLayout)findViewById(R.id.checkbox_content_layout);
-        container =  (LinearLayout)findViewById(R.id.fragment_container);
-        share_btn = (ImageButton)findViewById(R.id.checkbox_content_layout_share_btn);
-        delete_btn = (ImageButton)findViewById(R.id.checkbox_content_layout_delete_btn);
-        select_all_checkboxes = (Button)findViewById(R.id.checkbox_content_layout_select_all);
-        checkBox_toolbar = (ImageButton)findViewById(R.id.checkbox_toolbar);
-        sortFiles = (ImageButton)findViewById(R.id.sort_toolbar);
-        showFiles = (Button)findViewById(R.id.allfiles_toolbar);
+        temp_linear_for_checkbox = (LinearLayout) findViewById(R.id.checkbox_content_layout);
+        container = (LinearLayout) findViewById(R.id.fragment_container);
+        share_btn = (ImageButton) findViewById(R.id.checkbox_content_layout_share_btn);
+        delete_btn = (ImageButton) findViewById(R.id.checkbox_content_layout_delete_btn);
+        select_all_checkboxes = (Button) findViewById(R.id.checkbox_content_layout_select_all);
+        checkBox_toolbar = (ImageButton) findViewById(R.id.checkbox_toolbar);
+        sortFiles = (ImageButton) findViewById(R.id.sort_toolbar);
+        showFiles = (Button) findViewById(R.id.allfiles_toolbar);
         share_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -129,21 +133,19 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View v) {
                 container.removeAllViewsInLayout();
-                switch (state){
+                switch (state) {
 
-                    case "default":
-                    {
+                    case "default": {
                         showFiles.setText(getResources().getString(R.string.showExcelFilesOnly));
                         state = "displayTextFilesOnly";
                         break;
                     }
-                    case "displayExcelFilesOnly" :
-                    {
+                    case "displayExcelFilesOnly": {
                         showFiles.setText(getResources().getString(R.string.allfiles));
                         state = "default";
                         break;
                     }
-                    case "displayTextFilesOnly":{
+                    case "displayTextFilesOnly": {
                         showFiles.setText(getResources().getString(R.string.showTextFilesOnly));
                         state = "displayExcelFilesOnly";
                         break;
@@ -151,34 +153,32 @@ public class MainActivity extends AppCompatActivity
                 }
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.nav_my_files)
-        {
-            startActivity(new Intent(MainActivity.this,MainActivity.class));
-        }
-        else if (id == R.id.nav_have_sent_files)
-        {
+        if (id == R.id.nav_my_files) {
+            startActivity(new Intent(MainActivity.this, MainActivity.class));
+        } else if (id == R.id.nav_have_sent_files) {
             FragmentManager fragmentManager = getFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
             SheredFiles sheredFiles = new SheredFiles();
             fragmentTransaction.add(R.id.fragment_container, sheredFiles, "HELLO");
             fragmentTransaction.commit();
-        }
-        else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_send) {
 
             Intent sendIntent = new Intent();
             sendIntent.setAction(Intent.ACTION_SEND);
@@ -186,9 +186,7 @@ public class MainActivity extends AppCompatActivity
             sendIntent.setType("text/plain");
             startActivity(sendIntent);
 
-        }
-        else if (id == R.id.nav_payment)
-        {
+        } else if (id == R.id.nav_payment) { //TODO
         }
 
 
@@ -197,4 +195,43 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        /*client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://ir.rastanco.rastanbarcodescanner.presenter/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);*/
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+       /* Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://ir.rastanco.rastanbarcodescanner.presenter/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();*/
+    }
 }
