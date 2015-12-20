2022d9d907da8f3d6be8809e12abd2c5260d7e73
CafeWriter <samaneh.samiee@gmail.com>
2015-12-20 12:44:55 +0330

    Some colors changed

--- app/src/main/java/ir/rastanco/rastanbarcodescanner/presenter/MainActivity.java
+++ app/src/main/java/ir/rastanco/rastanbarcodescanner/presenter/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends AppCompatActivity
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setBackgroundTintList(getColorStateList(R.color.colorLightBlue));
+//        fab.setBackgroundTintList(getColorStateList(R.color.colorLightBlue));
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
