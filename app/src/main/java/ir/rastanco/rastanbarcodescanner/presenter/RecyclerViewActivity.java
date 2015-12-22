9e78430e1956f0a260113f5e495e2ed9e31a400d
CafeWriter <samaneh.samiee@gmail.com>
2015-12-22 14:41:56 +0330

    2 activities created

---
+++ app/src/main/java/ir/rastanco/rastanbarcodescanner/presenter/RecyclerViewActivity.java
@@ -0,0 +1,16 @@
+package ir.rastanco.rastanbarcodescanner.presenter;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import ir.rastanco.rastanbarcodescanner.R;
+
+public class RecyclerViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+    }
+
+}
