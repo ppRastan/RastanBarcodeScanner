69004fd9303afbc08b90f5d871d9a3f33157e4d3
ParisaRashidi <parisaRashidiNezhad@gmail.com>
2016-01-11 10:43:20 +0330

    2 class created to handle navigationDrawer

---
+++ app/src/main/java/ir/rastanco/rastanbarcodescanner/presenter/BarcodeReading/FileEditor.java
@@ -0,0 +1,16 @@
+package ir.rastanco.rastanbarcodescanner.presenter.BarcodeReading;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import ir.rastanco.rastanbarcodescanner.R;
+
+public class FileEditor extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_editor);
+    }
+
+}
